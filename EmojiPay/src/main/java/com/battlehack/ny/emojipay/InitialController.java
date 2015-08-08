67b1993633a2d5257cfb96baf122956b4bfa990d
Mike Gicking <mgicking@gmail.com>
2015-08-08 15:41:31 -0400

    Added template JSP for all views;
    Added API key to Initial Controller;

--- EmojiPay/src/main/java/com/battlehack/ny/emojipay/InitialController.java
+++ EmojiPay/src/main/java/com/battlehack/ny/emojipay/InitialController.java
@@ -2,4 +2,13 @@ package com.battlehack.ny.emojipay;
 
 public class InitialController {
 
+	
+	private static BraintreeGateway gateway = new BraintreeGateway(
+		Environment.SANDBOX,
+	    merchantId:   'nr4j778pfv72vqcq',
+	    publicKey:    '8j257k6cm2kzbv3b',
+	    privateKey:   'e2dc17870612ac31c1afe533068bfa26'
+	});
+	
+	
 }
