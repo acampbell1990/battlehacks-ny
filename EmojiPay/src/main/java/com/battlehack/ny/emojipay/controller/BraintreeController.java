65268ab5d23b76d823efe2f6a61f577e227859fc
Andrew Campbell <andrewcampbell1990@gmail.com>
2015-08-08 15:51:14 -0400

    Updated package style and brain tree controller.

--- EmojiPay/src/main/java/com/battlehack/ny/emojipay/controller/BraintreeController.java
+++ EmojiPay/src/main/java/com/battlehack/ny/emojipay/controller/BraintreeController.java
@@ -1,9 +1,9 @@
-package com.battlehack.ny.emojipay;
+package com.battlehack.ny.emojipay.controller;
 
 import com.braintreegateway.BraintreeGateway;
 import com.braintreegateway.Environment;
 
-public class InitialController {
+public class BraintreeController {
 	
 	private static BraintreeGateway gateway = new BraintreeGateway(Environment.SANDBOX, 
 			"nr4j778pfv72vqcq",
