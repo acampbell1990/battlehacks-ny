d4d7c72f1880e66d2e5540fbee7bc0390a278504
Andrew Campbell <andrewcampbell1990@gmail.com>
2015-08-08 15:48:23 -0400

    Resolved compilation errors.

--- EmojiPay/src/main/java/com/battlehack/ny/emojipay/InitialController.java
+++ EmojiPay/src/main/java/com/battlehack/ny/emojipay/InitialController.java
@@ -1,14 +1,12 @@
 package com.battlehack.ny.emojipay;
 
-public class InitialController {
+import com.braintreegateway.BraintreeGateway;
+import com.braintreegateway.Environment;
 
+public class InitialController {
 	
-	private static BraintreeGateway gateway = new BraintreeGateway(
-		Environment.SANDBOX,
-	    merchantId:   'nr4j778pfv72vqcq',
-	    publicKey:    '8j257k6cm2kzbv3b',
-	    privateKey:   'e2dc17870612ac31c1afe533068bfa26'
-	});
-	
-	
+	private static BraintreeGateway gateway = new BraintreeGateway(Environment.SANDBOX, 
+			"nr4j778pfv72vqcq",
+			"8j257k6cm2kzbv3b",
+			"e2dc17870612ac31c1afe533068bfa26");
 }
