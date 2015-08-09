ccb829c0e0cbf4abcf41505b36ad4e331cc688de
Andrew Campbell <andrewcampbell1990@gmail.com>
2015-08-08 21:22:15 -0400

    Fixed twitter controller to use oAuth.

--- EmojiPay/src/main/java/com/battlehack/ny/emojipay/controller/TwitterController.java
+++ EmojiPay/src/main/java/com/battlehack/ny/emojipay/controller/TwitterController.java
@@ -10,61 +10,77 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 import com.battlehack.ny.emojipay.model.MyAccessToken;
 import com.battlehack.ny.emojipay.model.OAuthToken;
 
 @Controller
 public class TwitterController {
-	 @Autowired
-	 private OAuthToken oauthToken;
-	     
-	 @Autowired
-	 private MyAccessToken accessToken; 
+	Twitter twitter = new TwitterFactory().getInstance();
+
+	@Autowired
+	private OAuthToken oauthToken;
+
+	@Autowired
+	private MyAccessToken accessToken; 
 	Logger LOG = Logger.getLogger(TwitterController.class);
-	
-/*
-	@RequestMapping(value="/success")
-	public void callback(HttpSession session, String oauth_token, String oauth_verifier) {
-		AccessToken accessToken = new AccessToken("1671148752-QjkSORzvS630VddhnZhRkYqyKE1IzPtTF4F23As",
-				"doISTZsYxjFGppVCeiQHQCdThQg9jAXztC37LTOKk9T3P");
-		Twitter twitter = new TwitterFactory().getInstance(accessToken);
-		 AccessToken accTok = null;
-		 try {
-			 accTok = twitter.getOAuthAccessToken();
-		 } catch (Exception e) {
-		    e.printStackTrace();
-		 }
-		
-		ResponseList<DirectMessage> status = null;
+
+	private final String CONSUMER_KEY = "zfIKyZf2aladF7ooaBMlCmAwP";
+	private final String CONSUMER_SECRET = "uh7Kgd3DOeo8KKNWFVXYQbw4DGK9fXZ2poHGfMTMZjG9J6gPzG";
+
+
+	@RequestMapping(value="/twitterAuth") 
+	public String printWelcome(HttpServletResponse response,HttpServletRequest request) {
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey(CONSUMER_KEY);
+		builder.setOAuthConsumerSecret(CONSUMER_SECRET);
+		Configuration configuration = builder.build();
+		TwitterFactory factory = new TwitterFactory(configuration);
+		Twitter twitter = factory.getInstance();
+		RequestToken requestToken;
+		String authUrl = null;
 		try {
-			status = twitter.getDirectMessages();
+			String callbackURL = "http://127.0.0.1:8080/EmoticonPayments/success";
+			requestToken = twitter.getOAuthRequestToken(callbackURL);
+			String token = requestToken.getToken();
+			String tokenSecret = requestToken.getTokenSecret();
+			accessToken.setTokensecret(tokenSecret);
+			accessToken.setToken(token);
+			authUrl = requestToken.getAuthorizationURL();
+			request.setAttribute("authUrl", authUrl);
 		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
+			LOG.info(e.getMessage());
 			e.printStackTrace();
 		}
-		LOG.info(status.get(0));
-		System.out.println(status.get(0));
+		return "redirect:" + authUrl;
 	}
-*/
-	    @RequestMapping(value="/success")
-	    protected ModelAndView handleRequestInternal(HttpServletRequest request,
-	            HttpServletResponse response) throws Exception {
-	            Twitter twitter = new TwitterFactory().getInstance();
-	             
-	            twitter.setOAuthConsumer(oauthToken.getConsumerKey(), oauthToken.getConsumerSecret());
-	            String verifier = request.getParameter("oauth_verifier");
-	           RequestToken requestToken = new RequestToken(accessToken.getToken(), accessToken.getTokensecret());
-	           AccessToken accessToken = twitter.getOAuthAccessToken(requestToken,verifier);
-	           twitter.setOAuthAccessToken(accessToken);
-	           User user = twitter.verifyCredentials();
-	           System.out.println(user.getName());
-	           ModelAndView model = new ModelAndView("loggedIn");
-	           model.addObject("message", user.getName());
-	           return model;
-	    }
+	
+	@RequestMapping(value="/success")
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response){
+		Twitter twitter = new TwitterFactory().getInstance();
+		String verifier = request.getParameter("oauth_verifier");
+		RequestToken requestToken = new RequestToken(accessToken.getToken(), accessToken.getTokensecret());
+		AccessToken accessToken;
+		ModelAndView model = null;
+		try {
+			accessToken = twitter.getOAuthAccessToken(requestToken,verifier);
+			twitter.setOAuthAccessToken(accessToken);
+			User user = twitter.verifyCredentials();
+			System.out.println(user.getName());
+			model = new ModelAndView("loggedIn");
+			model.addObject("message", user.getName());
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			LOG.info(e.getMessage());
+		}
+		return model;
+	}
 }
--- EmojiPay/src/main/webapp/WEB-INF/index.jsp
+++ EmojiPay/src/main/webapp/WEB-INF/index.jsp
@@ -10,6 +10,7 @@
 	<h2>Tweet and Eat! It's that simple. <b>:)</b></h2>
 	<div class="container">
 		<p>Welcome to EmojiPAY, the one and only service that lets you tweet your favorite orders and pay them automatically, ready for you to pick up or have delivered as fast as you can type!</p>
+		<a href='<%=request.getAttribute("authUrl") %>'>Sign in with Twitter</a>
 	</div>
 </body>
 </html>
